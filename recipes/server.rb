443e2db50f2d4de4011fecbc19f9cde282ca593a
jls-gibhut <souzajohnlee@gmail.com>
2021-12-27 19:50:22 +0000

    Version 0.2.0 - Display node data on HTML page

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,12 @@ package 'httpd' do
 end
 
 file 'var/www/html/index.html' do
-  content "<h1>Hello World</h1>"
+  content "<h1>Hello World</h1>
+  IP Address: #{node['ipaddress']}
+  Hostname: #{node['hostname']}
+  Memory: #{node['memory']['total']}
+  CPU: #{node['cpu']['0']['mhz']}
+"
 end
 
 service 'httpd' do
